171e82e0d04e8ada505e02381319c121ebc1222b
jpadriano <patrick_adriano4@yahoo.com>
2018-11-18 20:48:31 +0800

    setup.sh

--- setup.bat
+++ setup.bat
@@ -16,18 +16,33 @@ REM then
     REM echo "NODE_ENV exists. No need to drop the database.";
 REM else    
     REM # -- Enter database setup commands below -- #
-	     mysql -u root -e "CREATE DATABASE victory" -p
-    echo "Please enter mySQL password for the following"
+	     REM mysql -u root -e "CREATE DATABASE victory" -p
+    REM echo "Please enter mySQL password for the following"
      REM mysql -u root -e "DROP DATABASE IF EXISTS victory" -p
-    echo "Database dropped. Create new victory_db database"
-     mysql -u root -e "CREATE DATABASE victory" -p
-    echo "Database created. Create user victory. Will prompt an error if user already exists"
-     mysql -u root -e "CREATE USER 'victory'@'%' IDENTIFIED BY 'v1ct0ry'" -p
-    echo "User victory created. Grant privileges to victory for development."
-     mysql -u root -e "GRANT ALL PRIVILEGES ON *.* TO 'victory'@'%' WITH GRANT OPTION" -p
-    echo "Privileges granted. Flush privileges"
-     mysql -u root -e "FLUSH PRIVILEGES" -p;
-REM fi
+    REM echo "Database dropped. Create new victory_db database"
+     REM mysql -u root -e "CREATE DATABASE victory" -p
+    REM echo "Database created. Create user victory. Will prompt an error if user already exists"
+     REM mysql -u root -e "CREATE USER 'victory'@'%' IDENTIFIED BY 'v1ct0ry'" -p
+    REM echo "User victory created. Grant privileges to victory for development."
+     REM mysql -u root -e "GRANT ALL PRIVILEGES ON *.* TO 'victory'@'%' WITH GRANT OPTION" -p
+    REM echo "Privileges granted. Flush privileges"
+     REM mysql -u root -e "FLUSH PRIVILEGES" -p;
+fi
 
 echo "Creating tables for the database"
 sequelize db:migrate
+
+
+    REM # -- Enter database setup commands below -- #
+	     REM mysql -u newuser -e "CREATE DATABASE victory" -p
+    echo "Please enter mySQL password for the following"
+     REM mysql -u newuser -e "DROP DATABASE IF EXISTS victory" -p
+    echo "Database dropped. Create new victory_db database"
+     mysql -u newuser -e "CREATE DATABASE victory" -p
+    echo "Database created. Create user victory. Will prompt an error if user already exists"
+     mysql -u newuser -e "CREATE USER 'victory'@'%' IDENTIFIED BY 'v1ct0ry'" -p
+    echo "User victory created. Grant privileges to victory for development."
+     mysql -u newuser -e "GRANT ALL PRIVILEGES ON *.* TO 'victory'@'%' WITH GRANT OPTION" -p
+    echo "Privileges granted. Flush privileges"
+     mysql -u newuser -e "FLUSH PRIVILEGES" -p;
+	 sequelize db:migrate
